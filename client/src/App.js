5849c41a4ae7f08d208d69ec436d47f9f7d0c886
Neeraj Kumar <testingdach@gmail.com>
2024-03-08 13:07:37 +0530

    Updating

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 
+import {BrowserRouter as Router ,Routes, Route } from 'react-router-dom'
+import Chats from './components/Chats'
+import Home from  './components/Home'
+import "./App.css"
+
 
 function App() {
   return (
-    <div>
-       harry Potter
-    </div>
+     <Router>
+         <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/chats' element={<Chats/>} />
+            
+         </Routes>
+     </Router>
   );
 }
 
